51e43e7235cb42a4438a7abf1661ad95285fb3dd
LoveleenGoyal <loveleengoyal26@gmail.com>
2025-04-02 11:28:09 +0530

    [Loveleen] ADD: validat ssn

---
+++ src/main/java/com/bridgelabz/advancedRegex/RepeatedWords.java
@@ -0,0 +1,19 @@
+package com.bridgelabz.advancedRegex;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class RepeatedWords {
+    public static void main(String[] args) {
+        String regex = "\\b(\\w+)(\\s+\\1\\b)";
+
+        String text = "This is is a repeated repeated word test.";
+
+        Pattern pattern = Pattern.compile(regex,Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(text);
+
+        while (matcher.find()) {
+            System.out.println(matcher.group(1));
+        }
+    }
+}
